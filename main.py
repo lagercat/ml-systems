1f4b0e667a5546641b4e312b10dcc9ca2114a209
MalesSebastian <malessebastian@gmail.com>
2018-06-18 15:52:29 +0300

    Fix infinite loss

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ DATA_COLUMNS = ['Hobby', 'OpenSource', 'Country', 'Student',
                 'Employment', 'FormalEducation', 'UndergradMajor',
                 'YearsCodingProf', 'YearsCoding', 'CareerSatisfaction',
                 'JobSatisfaction', 'Salary']
-COUNTRIES = []
-
+norm_factor = 0
 
 def convert_salary(x):
     x = x.replace(",", "")
@@ -20,6 +19,10 @@ def load_data():
     data = data.dropna()
 
     data['Salary'] = data['Salary'].apply(convert_salary)
+
+    max_sal = data['Salary'].max() 
+    min_sal = data['Salary'].min()
+    norm_factor = max_sal - min_sal
 
     rows_in_train = int(0.7 * data.shape[0])
     train = data.iloc[0:rows_in_train]
@@ -33,36 +36,44 @@ def load_data():
 
 def train_input(features, results):
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), results))
-    dataset = dataset.shuffle(1000).batch(1).repeat()
-
+    dataset = dataset.batch(10)
     return dataset.make_one_shot_iterator().get_next()
 
-
+'''
 def get_unique_values(*args):
     list_of_all_values = []
     for argument in args:
         list_of_all_values += argument.values.T.tolist()
     return list(set(list_of_all_values))
-
+'''
 
 def main(argv):
     (train_x, train_y), (test_x, test_y) = load_data()
-
+   
     feature_columns = []
     for key in train_x.keys():
-        feature_columns.append(
-            tf.feature_column.categorical_column_with_vocabulary_list(
-                key=key,
-                vocabulary_list=get_unique_values(train_x[key],
-                                                  test_x[key]))
-        )
-    classifier = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+        column = tf.feature_column.categorical_column_with_hash_bucket(
+                key=key, hash_bucket_size=100)
+        feature_columns.append(tf.feature_column.embedding_column(column,
+            dimension=3))
 
-    classifier.train(
+    model = tf.estimator.DNNRegressor(hidden_units=[20, 20, 20],
+            feature_columns=feature_columns)
+    model.train(
         input_fn=lambda:train_input(train_x, train_y),
-        steps=10000)
+        steps=1000)
+
+    eval_result = model.evaluate(input_fn=lambda: train_input(test_x, test_y))
+    average_loss = eval_result['average_loss']
+    print("\n" + 80 * "*")
+    print("\nRMS error for the test set: ${:.0f}"
+          .format(norm_factor * average_loss**0.5))
+
+    print()
+
 
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main)
+
