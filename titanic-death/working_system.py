fbcb7f839a11d636488e8450274aec73f12b8d47
MalesSebastian <malessebastian@gmail.com>
2018-06-29 17:10:15 +0300

    Delete parameters testing

--- titanic-death/working_system.py
+++ titanic-death/working_system.py
@@ -37,45 +37,28 @@ def main(argv):
 
     (train_x, train_y), (test_x, test_y) = load_data()
 
-    number_of_units = [10, 15, 20, 25, 30, 35, 40]
-    learning_rate = [0.01, 0.03, 0.1, 0.3, 1, 3]
-    best_units = 0
-    best_rate = 0
-    best_error = 2 ** 60
+    units = 2 * [30]
+    optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
+    model = tf.estimator.DNNClassifier(hidden_units=units,
+                                       feature_columns=feature_columns,
+                                       optimizer=optimizer,
+                                       activation_fn=tf.nn.sigmoid)
 
-    for rate in learning_rate:
-        for unit in number_of_units:
-            units = 2 * [unit]
-            optimizer = tf.train.AdagradOptimizer(learning_rate=rate)
-            model = tf.estimator.DNNClassifier(hidden_units=units,
-                                               feature_columns=feature_columns,
-                                               optimizer=optimizer,
-                                               activation_fn=tf.nn.sigmoid)
+    model.train(input_fn=lambda: inp(train_x, train_y, 'TRAIN'),
+                steps=50000)
 
-            model.train(input_fn=lambda: inp(train_x, train_y, 'TRAIN'),
-                        steps=20000)
+    eval_result = model.evaluate(input_fn=lambda: inp(test_x, test_y,
+                                 'EVAL'))
+    average_loss = eval_result['average_loss']
 
-            eval_result = model.evaluate(input_fn=lambda: inp(test_x, test_y,
-                                         'EVAL'))
-            average_loss = eval_result['average_loss']
+    print('Average loss: ' + str(average_loss))
 
-            print('Average loss: ' + str(average_loss))
-            if average_loss < best_error:
-                best_error = average_loss
-                best_units = number_of_units
-                best_rate = rate
-
-    print('Best units:', best_units)
-    print('Best rate:', best_rate)
-
-    '''
     brute_results = model.predict(input_fn=lambda: inp(load_submit(),
                                   (), 'PREDICT'))
     net_results = []
     for line in brute_results:
         net_results.append(line['class_ids'][0])
     write_to_file(net_results)
-    '''
 
 
 if __name__ == '__main__':
