ff2d8fa3687de3414c1023b140fe86a3c2fbd65c
MalesSebastian <malessebastian@gmail.com>
2018-06-29 15:16:36 +0300

    Add feature engineering

--- titanic-death/get_data.py
+++ titanic-death/get_data.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
-CSV_COLUMNS = ['Survived', 'Pclass', 'Sex', 'Age',
-               'Fare', 'Embarked', 'SibSp', 'Parch']
+CSV_COLUMNS = ['Survived', 'Pclass', 'Sex', 'Age', 'Cabin',
+               'Name', 'Fare', 'Embarked', 'SibSp', 'Parch']
 
 
 DATA = '~/Datasets/'
@@ -17,18 +18,63 @@ fill_dict = {
     'SibSp': 0,
     'Parch': 0,
     'agcl': 0,
-    'fsize': 0
+    'fsize': 0,
+    'title': '',
+    'Cabin': '',
+    'deck': '',
+    'Fare_Per_Person': 0
 }
+
+title_list = ['Mrs', 'Mr', 'Master', 'Miss', 'Major', 'Rev',
+              'Dr', 'Ms', 'Mlle', 'Col', 'Capt', 'Mme', 'Countess',
+              'Don', 'Jonkheer']
+cabin_list = ['A', 'B', 'C', 'D', 'E', 'F', 'T', 'G', '']
+
+
+def replace_titles(x):
+    title = x['title']
+    if title in ['Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col']:
+        return 'Mr'
+    elif title in ['Countess', 'Mme']:
+        return 'Mrs'
+    elif title in ['Mlle', 'Ms']:
+        return 'Miss'
+    elif title == 'Dr':
+        if x['Sex'] == 'Male':
+            return 'Mr'
+        else:
+            return 'Mrs'
+    else:
+        return title
+
+
+def substring_in_string(big_string, substrings):
+    if big_string is not np.nan:
+        for substring in substrings:
+            if substring in big_string or substring.lower() in big_string:
+                return substring
+    return np.nan
+
+
+def feature_engineering(data):
+    data['agcl'] = data['Age'] * data['Pclass']
+    data['fsize'] = data['SibSp'] + data['Parch']
+    data['title'] = data['Name'].map(lambda x: substring_in_string(x,
+                                     title_list))
+    data['title'] = data.apply(replace_titles, axis=1)
+    data['deck'] = data['Cabin'].map(lambda x: substring_in_string(x,
+                                     cabin_list))
+    data['Fare_Per_Person'] = data['Fare']/(data['fsize']+1)
+    data = data.drop('Name', 1)
+    data = data.drop('Cabin', 1)
+    data = data.fillna(fill_dict)
+    return data
 
 
 def load_data(ratio=0.7, data=812):
     data = pd.read_csv(DATASET, usecols=CSV_COLUMNS,
                        header=0)
-    data['agcl'] = data['Age'] * data['Pclass']
-    data['fsize'] = data['SibSp'] + data['Parch']
-
-    data = data.fillna(fill_dict)
- 
+    data = feature_engineering(data)
     rows = data.shape[0]
 
     train_data = data.head(int(ratio * rows))
@@ -43,9 +89,7 @@ def load_data(ratio=0.7, data=812):
 def load_submit():
     data = pd.read_csv(SUBMIT_DATASET, usecols=CSV_COLUMNS[1:],
                        header=0)
-    data['agcl'] = data['Age'] * data['Pclass']
-    data['fsize'] = data['SibSp'] + data['Parch']
-    data = data.fillna(fill_dict)
+    data = feature_engineering(data)
     return data
 
 
--- titanic-death/working_system.py
+++ titanic-death/working_system.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
-from get_data import inp, load_data, load_submit, write_to_file
+from get_data import inp, load_data, load_submit, write_to_file, cabin_list
 
 
 def main(argv):
-
 
     feature_columns = [
         tf.feature_column.indicator_column(
@@ -22,35 +21,61 @@ def main(argv):
         tf.feature_column.numeric_column(key='SibSp'),
         tf.feature_column.numeric_column(key='Parch'),
         tf.feature_column.numeric_column(key='agcl'),
-        tf.feature_column.numeric_column(key='fsize')
+        tf.feature_column.numeric_column(key='fsize'),
+        tf.feature_column.embedding_column(
+            tf.feature_column.categorical_column_with_vocabulary_list(
+                key='title',
+                vocabulary_list=['Mr', 'Mrs', 'Miss']),
+            dimension=3),
+        tf.feature_column.embedding_column(
+            tf.feature_column.categorical_column_with_vocabulary_list(
+                key='deck',
+                vocabulary_list=cabin_list),
+            dimension=3),
+        tf.feature_column.numeric_column(key='Fare_Per_Person')
     ]
 
     (train_x, train_y), (test_x, test_y) = load_data()
 
-    units = 2 * [20]
-    optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
-    model = tf.estimator.DNNClassifier(hidden_units=units,
-                                       feature_columns=feature_columns,
-                                       optimizer=optimizer,
-                                       activation_fn=tf.nn.sigmoid)
+    number_of_units = [10, 15, 20, 25, 30, 35, 40]
+    learning_rate = [0.01, 0.03, 0.1, 0.3, 1, 3]
+    best_units = 0
+    best_rate = 0
+    best_error = 2 ** 60
 
-    model.train(input_fn=lambda: inp(train_x, train_y, 'TRAIN'),
-                steps=30000)
+    for rate in learning_rate:
+        for unit in number_of_units:
+            units = 2 * [unit]
+            optimizer = tf.train.AdagradOptimizer(learning_rate=rate)
+            model = tf.estimator.DNNClassifier(hidden_units=units,
+                                               feature_columns=feature_columns,
+                                               optimizer=optimizer,
+                                               activation_fn=tf.nn.sigmoid)
 
-    eval_result = model.evaluate(input_fn=lambda: inp(test_x, test_y,
-                                 'EVAL'))
-    average_loss = eval_result['average_loss']
+            model.train(input_fn=lambda: inp(train_x, train_y, 'TRAIN'),
+                        steps=20000)
 
+            eval_result = model.evaluate(input_fn=lambda: inp(test_x, test_y,
+                                         'EVAL'))
+            average_loss = eval_result['average_loss']
 
-    
-    print('Average loss: ' + str(average_loss))
+            print('Average loss: ' + str(average_loss))
+            if average_loss < best_error:
+                best_error = average_loss
+                best_units = number_of_units
+                best_rate = rate
 
+    print('Best units:', best_units)
+    print('Best rate:', best_rate)
+
+    '''
     brute_results = model.predict(input_fn=lambda: inp(load_submit(),
                                   (), 'PREDICT'))
     net_results = []
     for line in brute_results:
         net_results.append(line['class_ids'][0])
     write_to_file(net_results)
+    '''
 
 
 if __name__ == '__main__':
